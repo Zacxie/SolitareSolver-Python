5cfedb49906a0e914d6b5d7c7b9ea868ac63eb7b
Idkirsch <s195483@student.dtu.dk>
2021-04-14 10:08:30 +0200

    åbner webcam i grayscale

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import cv2
 
+cam = cv2.VideoCapture(0)
 image = cv2.imread('cards.jpg')
 
-# make it grayscale
-Gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+while True:
+    check, frame = cam.read()
 
-# Make canny Function
-canny=cv2.Canny(Gray,40,140)
+    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('video', grayFrame)
 
-# the threshold is varies bw 0 and 255
-cv2.imshow("Canny",canny)
-cv2.waitKey(0)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cam.release()
 cv2.destroyAllWindows()
 
+
+
+# # make it grayscale
+# Gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+#
+# # Make canny Function
+# canny=cv2.Canny(Gray,40,140)
+#
+# # the threshold is varies bw 0 and 255
+# cv2.imshow("Canny",canny)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+#
 
 
 
