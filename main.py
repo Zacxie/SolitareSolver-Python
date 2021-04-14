d0f94003c2929852b3ba76f49a7d94683262562f
Idkirsch <s195483@student.dtu.dk>
2021-04-14 12:35:52 +0200

    detecter fra webcam ( fejl )

--- main.py
+++ main.py
@@ -1,34 +1,79 @@
 import cv2
+import numpy as np
 
-cam = cv2.VideoCapture(0)
-image = cv2.imread('cards.jpg')
+
+net = cv2.dnn.readNet('yolov3-tiny.weights', 'yolov3-tiny.cfg')
+classes = []
+with open('coco.names', 'r') as f:
+    classes = f.read().splitlines()
+
+threshold = 0.5
+
+print(classes)
+
+cap = cv2.VideoCapture(0)
+# img = cv2.imread('image.jpg')
 
 while True:
-    check, frame = cam.read()
+    _,img = cap.read()
+    height, width, _ = img.shape
 
-    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('video', grayFrame)
+    blob = cv2.dnn.blobFromImage(img, 1/255, (416,416), (0,0,0), swapRB=True, crop=False)
+    net.setInput(blob)
+    output_layers_names = net.getUnconnectedOutLayersNames()
+    layerOutputs = net.forward(output_layers_names)
 
-    key = cv2.waitKey(1)
-    if key == 27:
+    # for b in blob:
+    #     for n, img_blob in enumerate(b):
+    #         cv2.imshow(str(n), img_blob)
+
+    boxes = []
+    confidences = []
+    class_ids = []
+
+    for output in layerOutputs:
+        for detection in output:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id] #22:49 - ikke sikkert
+            if confidence > threshold:
+                center_x = int(detection[0]*width)
+                center_y = int(detection[1]*height)
+                w = int(detection[2]*width)
+                h = int(detection[3]*height)
+
+                x = int(center_x - w/2)
+                y = int(center_y - h/2)
+
+                boxes.append([x, y, w, h])
+                confidences.append((float(confidence)))
+                class_ids.append(class_id)
+
+    #removing redundant boxes
+    print(len(boxes))
+    indexes = cv2.dnn.NMSBoxes(boxes,confidences,threshold, 0.4)
+    print(indexes.flatten())
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    colors = np.random.uniform(0,255,size=(len(boxes),3))
+
+
+    for i in indexes.flatten:
+        x,y,w,h = boxes[i] #identify each object and extract information
+        label = str(classes[class_ids[i]])
+        confidence = str(round(confidences[i],2))
+        color = colors[i]
+        cv2.rectangle(img,(x,y),(x+w,y+h),color,2)
+        cv2.putText(img,label +" "+confidence,(x,y+20),font,2,(255,255,255),2)
+
+
+
+    cv2.imshow('Image', img)
+    key= cv2.waitKey(1)
+    if key==27:
         break
 
-cam.release()
+cap.release()
 cv2.destroyAllWindows()
 
-
-
-# # make it grayscale
-# Gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-#
-# # Make canny Function
-# canny=cv2.Canny(Gray,40,140)
-#
-# # the threshold is varies bw 0 and 255
-# cv2.imshow("Canny",canny)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-#
-
-
-
+# https://www.youtube.com/watch?v=1LCb1PVqzeY&ab_channel=eMasterClassAcademy
