771cdefac5dc7d5a77481f87bad73e2e244e76e4
Marie <S185363@student.dtu.dk>
2021-04-28 11:03:23 +0200

    Socket communication

--- client.py
+++ client.py
@@ -3,19 +3,36 @@ import socket
 from past.builtins import raw_input
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(("localhost", 5000))
+#client_socket.connect(("localhost", 5000))
 #client_socket.bind(("localhost", 5000))
 cardArray = ["AH", "QH", "KH", "10H", "6H", "5S", "3S"]
 
-while True:
-    data = raw_input("Starting session")
+try:
+    client_socket.bind(("localhost",9999))
+except socket.error as err:
+    print('bind failed. Error: ' .format(err))
+client_socket.listen(10)
+print("socket listening")
+conn, addr = client_socket.accept()
 
-    if data != 'Q' and data != 'q':
-        data = ' '.join([str(elem) for elem in cardArray])
-        data = data + "\n"
-        client_socket.send(data.encode('utf-8'))
-    else:
-        data = data + "\n"
-        client_socket.send(data.encode('utf-8'))
-        client_socket.close()
-        break
+i =0
+
+while(True):
+    conn.send(bytes("Message "+str(i)+" \r\n",'UTF-8'))
+    print("Message sent "+ str(i))
+    data = conn.recv(1024)
+    print(data.decode(encoding='UTF-8'))
+    i=i+1
+
+# while True:
+#     data = raw_input("Starting session")
+#
+#     if data != 'Q' and data != 'q':
+#         data = ' '.join([str(elem) for elem in cardArray])
+#         data = data + "\n"
+#         client_socket.send(data.encode('utf-8'))
+#     else:
+#         data = data + "\n"
+#         client_socket.send(data.encode('utf-8'))
+#         client_socket.close()
+#         break
