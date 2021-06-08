746ca393d937e425efbb3fdbfffbe77b9e698479
peterrevsbech <Peter.revsbech@gmail.com>
2021-06-08 15:54:06 +0200

    Har refaktureret stateRecognizer.py så den nu bruger dictionaries - det virker noget bedre, men vi har ikke teste det desuden fundet et par fejl i hvordan "processed" arrayet virkede

--- stateRecognizer.py
+++ stateRecognizer.py
@@ -10,124 +10,90 @@ class StateRecognizer(object):
         self.y_buffer_top = int(height * 0.30)
         self.y_buffer_bottom = int(height * 0.35)
         self.acceptance_radius = self.x_pile_witdh
-        self.itemLabels = []
-        self.processed = []
-        self.x = []
-        self.y = []
-        self.count = []
+        self.processed = {}
+        self.x = {}
+        self.y = {}
+        self.count = {}
         self.ready = False
 
-
-
     def reset(self):
-        self.itemLabels = []
-        self.processed = []
-        self.x = []
-        self.y = []
-        self.count = []
+        self.processed = {}
+        self.x = {}
+        self.y = {}
+        self.count = {}
         self.ready = False
 
     def resetTurn(self):
-        # Remove all unprocessed items
-        self.ready = False
-        newItemLabels = []
-        newX = []
-        newY = []
-        for i in range(len(self.itemLabels)):
-            if self.processed[i]:
-                newItemLabels.append(self.itemLabels[i])
-                newX.append(self.x[i])
-                newY.append(self.y[i])
-
-        self.itemLabels = newItemLabels
-        self.x = newX
-        self.y = newY
-        self.processed = []
-        # Mark all the remaining items as processed
-        for i in range(len(self.itemLabels)):
-            self.processed.append(True)
-            #self.processed[i] = True
+        for key in self.processed.items():
+            self.processed[key] = True
 
     def addItem(self, label, x, y):
-        is_contained = False
-        for i in range(len(self.itemLabels)):
-            # If card is already recognized.
-            if self.itemLabels[i] == label:
-                self.x[i] = x
-                self.y[i] = y
-                self.count[i] = self.count[i] + 1
-                is_contained = True
-
-        if not is_contained:
-            # If the card is new and not have been recognized before
-            self.itemLabels.append(label)
-            self.processed.append(False)
-            self.x.append(x)
-            self.y.append(y)
-            self.count.append(1)
-
+        if label in self.processed.keys():
+            #Increment it's counter and update coordinates
+            self.count[label] = self.count[label] + 1
+            self.x[label] = x
+            self.y[label] = y
+        else:
+            #Add entry to all hashmaps
+            self.count[label] = 1
+            self.x[label] = x
+            self.y[label] = y
+            self.processed[label] = False
 
 
     def evaluateFirstRound(self):
         # In the first round - we assume, that it has recognized 7 cards and we want these sorted with regards to x-value
-        if len(self.itemLabels) < 7:
+        if len(self.processed.keys()) < 7:
             return "ERROR - LESS THAN 7 CARDS"
 
-        # Sort after count
-        _, sorted_labels = zip(*sorted(zip(self.count, self.itemLabels)))
+        #Find keys of 7 highest counts
+        keys = sorted(self.count, key=self.count.get, reverse=True)[:7]
 
-        # Take 7 most counted cards
-        sorted_labels = reverse(sorted_labels)[0:7]
-
-        # Create new x vector - only with cards in sorted_labels
-        newX = []
-        for i in range(len(sorted_labels)):
-            index = self.itemLabels.index(sorted_labels[i])
-            newX.append(self.x[index])
+        xvals = []
+        labels = []
+        for key in keys:
+            xvals.append(self.x[key])
+            labels.append(key)
 
         # Sort after x value
-        _, sorted_labels = zip(*sorted(zip(newX, sorted_labels)))
+        _, labels = zip(*sorted(zip(xvals, labels)))
 
-        # Go through items. Set count to 0 if card was an error. Otherwise mark as processed.
-        for i in range(len(self.itemLabels)):
-            if self.itemLabels[i] not in sorted_labels:
-                self.count[i] = 0
-            else:
-                self.processed[i] = True
+        # Delete all unrecognized cards
+        for key in self.processed.keys():
+            if key not in labels:
+                self.count[key] = 0
 
-        # Return 7 most counted cards sorted after x value
-        return sorted_labels
+        return labels
 
     def evaluate(self):
         # To be called if a new card was revealed to figure out which card it was
         # Looks at all recognized cards - finds the one that was not known in previous round and also have been recognized the most times
-        # The remaining cards that hasn't been recognized will there count be set to 0.
+        # The counts of the remaining cards will be set to 0.
 
         maxCount = 0
-        index = None
+        maxKey = None
 
-        # Find the card with the highest count (recognized most times)
-        for i in range(len(self.processed)):
-            if not self.processed[i]:
+        for key,value in self.count.items():
+            # Find most counted unprocessed card
+            if value > maxCount and not self.processed[key]:
+                maxCount = value
+                maxKey = key
 
-                if maxCount < self.count[i]:
-                    maxCount = self.count[i]
-                    index = i
+        # Delete all unrecognized cards
+        for key in self.processed.keys():
+            # If not previously processed or the newly found max key - delete it
+            if key != maxKey or not self.processed[key]:
+                del self.count[key]
+                del self.processed[key]
+                del self.x[key]
+                del self.y[key]
 
-        self.processed[index] = True
-
-        # Sets the count for all the cards that hasn't been processed to 0 (The cards that most likely are errors)
-        for i in range(len(self.processed)):
-            if not self.processed[i]:
-                self.count[i] = 0
-
-        return self.itemLabels[index]
+        return maxKey
 
     def isReady(self, expected):
-        return len(self.itemLabels) >= expected
+        return len(self.processed.keys()) >= expected
 
 
 def reverse(lst):
     return [ele for ele in reversed(lst)]
-
 
