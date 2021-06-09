ae1d3f8d788b76b83b2e5a96ab26ce02ba33cc5e
Marie <S185363@student.dtu.dk>
2021-06-09 11:27:31 +0200

    start ide til suits besked

---
+++ convertCards.py
@@ -0,0 +1,16 @@
+
+class conversion(object):
+
+    def converCards(self):
+
+        for i in newCards:
+            if (i == 'AH'):
+                printarray.append('A♥')
+            elif (i == '2H'):
+                printarray.append('2♥')
+
+
+
+
+
+    converCards()
