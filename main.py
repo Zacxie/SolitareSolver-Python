6bb54d198027779121d3706a769da6e91d6c22ce
Mads Storgaard-Nielsen <s180076@student.dtu.dk>
2021-06-10 09:13:10 +0200

    updated some strings

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ Demo program that displays a webcam using OpenCV
 # constants
 noneMSG = 'NONE'
 
-def main():
 
+def main():
     sg.ChangeLookAndFeel('LightGreen')
 
     # define the window layout
@@ -30,10 +30,10 @@ def main():
               [sg.Text('Moves', size=(40, 1), justification='left', font='Helvetica 14')],
               [sg.Multiline(size=(30, 30), disabled=True, key='textbox', justification='top'),
                sg.Image(filename='', key='image')],
-              [sg.RButton('Start Capture', size=(10, 1),pad=((247, 0), 3), font='Any 14'),
+              [sg.RButton('Start Capture', size=(10, 1), pad=((247, 0), 3), font='Any 14'),
                sg.RButton('End Capture', size=(10, 1), font='Any 14'),
-               sg.RButton('New Game', size=(10, 1),pad=((150, 0), 3), font='Any 14'),
-               sg.ReadButton('Exit', size=(10, 1),  font='Helvetica 14')]]
+               sg.RButton('New Game', size=(10, 1), pad=((150, 0), 3), font='Any 14'),
+               sg.ReadButton('Exit', size=(10, 1), font='Helvetica 14')]]
     # Initialize video capture and dimensions
     cap = cv.VideoCapture(0)
     _, frame = cap.read()  #
@@ -56,7 +56,7 @@ def main():
 
         button, values = gs.window.read(timeout=0)
 
-        #Button choice
+        # Button choice
         if button == 'Exit' or values is None:
             sys.exit(0)
 
@@ -89,17 +89,20 @@ def main():
         imgbytes = bio.getvalue()  # this can be used by OpenCV hopefully
         window.FindElement('image').Update(data=imgbytes)
 
+
 def gameWon(gs):
     gs.window['End Capture'].update(disabled=True)
     gs.window['Start Capture'].update(disabled=True)
     sg.popup_ok('Congratulations, you won the game! You can start a new game by selecting New Game, '
                 'or quit the game by selecting Exit. Thanks for playing!', keep_on_top=True)
 
+
 def gameLost(gs):
     gs.window['End Capture'].update(disabled=True)
     gs.window['Start Capture'].update(disabled=True)
     sg.popup_ok('You lost! Better luck next time. You can start a new game by selecting New Game, '
                 'or quit the game by selecting Exit. Thanks for playing!', keep_on_top=True)
+
 
 def newGame(gs):
     gs.newGamePressed = True
@@ -111,14 +114,15 @@ def newGame(gs):
     gs.firstRound = True
     gs.numOfExpectedCards = 7
 
+
 def confirmFirstRound(gs):
     printarray = []
     gs.newCards = gs.recognizer.evaluateFirstRound()
     conversion.convertCards(gs.newCards, printarray)
-    return sg.popup_yes_no('Confirming state',
-                             'New cards this round were: ' + str(printarray),
-                             'Are you satisfied with the current state recognized?',
-                             keep_on_top=True)
+    return sg.popup_yes_no('New cards: ' + str(printarray),
+                           'Correct?',
+                           keep_on_top=True)
+
 
 def confirmOtherRounds(gs):
     printarray = []
@@ -128,12 +132,12 @@ def confirmOtherRounds(gs):
         conversion.convertSingle(gs.newCards, printarray)
         gs.numOfExpectedCards = gs.numOfExpectedCards + 1
 
-        return sg.popup_yes_no('Confirming state',
-                               'New card this round was: ' + str(printarray),
-                               'Are you satisfied with the current state recognized?',
+        return sg.popup_yes_no('New card: ' + str(printarray),
+                               'Correct?',
                                keep_on_top=True)
-    elif gs.newCards == None:
-        return sg.popup_ok('No new card was found. Try moving either the cards or camera around for better image.', keep_on_top=True)
+    elif gs.newCards is None:
+        return sg.popup_ok('No new card was found. Try moving either the cards or camera a bit.',
+                           keep_on_top=True)
 
 
 def onConfirmCards(gs):
@@ -162,6 +166,7 @@ def onConfirmCards(gs):
     elif msgItems[2] == "GAME_LOST":
         gameLost(gs)
 
+
 def endCapture(gs):
     gs.newCards = noneMSG
     answer = "Yes"
@@ -184,5 +189,6 @@ def endCapture(gs):
             gs.window['End Capture'].update(disabled=True)
 
     gs.newGamePressed = False
+
 
 main()
