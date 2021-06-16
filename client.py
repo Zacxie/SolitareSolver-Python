f3c6d29f76a102bc84fac9659db9a2d51560ead3
Marie <S185363@student.dtu.dk>
2021-06-16 11:56:06 +0200

    Copyright tilføjet

--- client.py
+++ client.py
@@ -1,3 +1,12 @@
+
+### Gruppe 12 ###
+#Christian Kyed - s184210
+#Ida Schrader - s195483
+#Mads Storgaard-Nielsen - s180076
+#Marie Seindal - s185363
+#Peter Revsbech - s183760
+#Sebastian Bjerre - s163526
+
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- conversion.py
+++ conversion.py
@@ -1,4 +1,11 @@
 
+### Gruppe 12 ###
+#Christian Kyed - s184210
+#Ida Schrader - s195483
+#Mads Storgaard-Nielsen - s180076
+#Marie Seindal - s185363
+#Peter Revsbech - s183760
+#Sebastian Bjerre - s163526
 
 def convertCards(newCards, printarray=[]):
 
